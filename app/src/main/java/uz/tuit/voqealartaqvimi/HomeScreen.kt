0a6ebc07b2cd8085deb17f391f70ce777e1c5f26
mahmud0v <islomibndilmurod@gmail.com>
2022-06-25 12:39:44 -0700

    add functionality

--- app/src/main/java/uz/tuit/voqealartaqvimi/HomeScreen.kt
+++ app/src/main/java/uz/tuit/voqealartaqvimi/HomeScreen.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.CalendarView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -32,6 +33,7 @@ class HomeScreen : Fragment() {
         val list = binding.calendarView
         val customFormat = SimpleDateFormat("dd/MM/yyyy")
         val currentDate = customFormat.format(binding.calendarView.date).substring(0, 2)
+        val numberCurrentDate = currentDate.toInt()
         val eventList = AppDatabase.db!!.eventDao()!!.getDateEvents(currentDate.toString())
         if (eventList.isNotEmpty()) {
             adapter = EventAdapter()
@@ -39,19 +41,47 @@ class HomeScreen : Fragment() {
             binding.rvView.adapter = adapter
             binding.rvView.layoutManager = LinearLayoutManager(requireContext())
         }
+        
+        if (numberCurrentDate==12|| numberCurrentDate in 1..2){
+            binding.faslId.text = "Qish"
+
+        }
+        if (numberCurrentDate in 3..5){
+            binding.faslId.text = "Bahor"
+        }
+        if (numberCurrentDate in 6..8){
+            binding.faslId.text = "Yoz"
+        }
+        if (numberCurrentDate in 9..11){
+            binding.faslId.text = "Kuz"
+        }
+
 
         binding.calendarView.setOnDateChangeListener(
             CalendarView.OnDateChangeListener { calendarView, i, i2, i3 ->
+
                 val anoEventList =
                     AppDatabase.db!!.eventDao()!!.getDateEvents(i3.toString())
                 val adapter = EventAdapter()
                 adapter.differ.submitList(anoEventList)
                 binding.rvView.adapter = adapter
                 binding.rvView.layoutManager = LinearLayoutManager(requireContext())
+                if ((i2+1)==12||(i2+1) in 1..2){
+                    binding.faslId.text = "Qish"
 
-
+                }
+                if ((i2+1) in 3..5){
+                    binding.faslId.text = "Bahor"
+                }
+                if ((i2+1)in 6..8){
+                    binding.faslId.text = "Yoz"
+                }
+                if ((i2+1) in 9..11){
+                    binding.faslId.text = "Kuz"
+                }
             }
         )
+
 
         binding.addBtn.setOnClickListener {
             findNavController().navigate(R.id.action_homeScreen_to_addEventScreen)
